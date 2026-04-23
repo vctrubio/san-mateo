58c2a32804b866742c2b1e4957c82665acbb4cea
vctrubio <vctrubio@gmail.com>
2026-04-23 23:44:42 +0200

    ui: update main landing page layout and hero design tokens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 import HeroLanding from "@/components/HeroLanding";
 import DebugColorPanel from "@/components/DebugColorPanel";
+import PropertyShowcase from "@/components/PropertyShowcase";
+import AboutSection from "@/components/AboutSection";
+import PropertyAvailability from "@/components/PropertyAvailability";
 
 export default function Home() {
   return (
     <main className="min-h-screen">
       <HeroLanding />
-      <DebugColorPanel />
+      <PropertyShowcase />
+      <PropertyAvailability />
+      <AboutSection />
+      {/* <DebugColorPanel /> */}
     </main>
   );
 }
